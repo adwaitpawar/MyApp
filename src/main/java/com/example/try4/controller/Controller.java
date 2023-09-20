4eb339435483732dfcb4e8f58b76906cb1b6bd09
adwait pawar <adwaitpawar@gmail.com>
2023-09-20 11:10:24 +0530

    first commit

---
+++ src/main/java/com/example/try4/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.example.try4.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    ModelAndView return1() {
+        ModelAndView mav = new ModelAndView("index");
+        return mav;
+    }
+}
