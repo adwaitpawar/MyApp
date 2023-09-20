2cf51ed8727756f7f0bd1681d3ae4ab0b65bdaee
adwaitpawar <110279221+adwaitpawar@users.noreply.github.com>
2023-09-20 15:55:51 +0530

    Update Try4Application.java

--- src/main/java/com/example/try4/Try4Application.java
+++ src/main/java/com/example/try4/Try4Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Try4Application {
+public class Try4Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Try4Application.class, args);
