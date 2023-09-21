e94bed2d2bead477d13dbabb5092e8edb011a310
adwaitpawar <110279221+adwaitpawar@users.noreply.github.com>
2023-09-21 12:54:49 +0530

    Update Controller.java

--- src/main/java/com/example/try4/controller/Controller.java
+++ src/main/java/com/example/try4/controller/Controller.java
@@ -11,7 +11,7 @@ public class Controller {
         ModelAndView mav = new ModelAndView("index");
         return mav;
     }
-    @GetMapping("/Adwait , method = RequestMethod.POST")
+    @GetMapping("/Adwait")
 	String return2(){
 		return "Hello AdWAit";
     }
